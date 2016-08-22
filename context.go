192348291755a70fa1678a53c9adde446a51d444
Kyle McCullough <kylemcc@gmail.com>
2016-08-22 16:34:39 -0500

    add method on context for retrieving the enviornment

--- context.go
+++ context.go
@@ -1,11 +1,34 @@
 package kubegen
 
 import (
+	"os"
+	"strings"
+	"sync"
+
 	kapi "k8s.io/kubernetes/pkg/api"
+)
+
+var (
+	envOnce sync.Once
+	envMap  map[string]string
 )
 
 type Context struct {
 	Pods      []kapi.Pod
 	Services  []kapi.Service
 	Endpoints []kapi.Endpoints
+}
+
+func (c *Context) Env() map[string]string {
+	envOnce.Do(loadEnv)
+	return envMap
+}
+
+func loadEnv() {
+	env := os.Environ()
+	envMap = make(map[string]string, len(env))
+	for _, v := range env {
+		vs := strings.SplitN(v, "=", 2)
+		envMap[vs[0]] = vs[1]
+	}
 }
