6fae4d2ec1820d43985916115cc9af633bdd9434
Pandu Aji <panaji@microsoft.com>
2018-01-10 15:05:07 -0800

    Fix need-fmt error

--- util_win.go
+++ util_win.go
@@ -3,23 +3,23 @@
 package kubegen
 
 import (
-    "fmt"
-    "os"
-    "os/exec"
+	"fmt"
+	"os"
+	"os/exec"
 )
 
 const SHELL_EXE = "cmd"
 const SHELL_ARG = "/c"
 
 func setFileModeAndOwnership(f *os.File, fi os.FileInfo) error {
-    return nil
+	return nil
 }
 
 func moveFile(src string, dest string) error {
-    moveCmd := "move " + src + " " + dest
-    cmd := exec.Command(SHELL_EXE, SHELL_ARG, moveCmd)
-    if err := cmd.Run(); err != nil {
-        return fmt.Errorf("error creating output file: %v", err)
-    }
-    return nil
+	moveCmd := "move " + src + " " + dest
+	cmd := exec.Command(SHELL_EXE, SHELL_ARG, moveCmd)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error creating output file: %v", err)
+	}
+	return nil
 }
