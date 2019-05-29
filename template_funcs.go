ed5c81ceeebacd8079040f531e9d3ed5f84ff657
Phil Madden <philmadden83@gmail.com>
2019-05-29 15:30:07 -0500

    Adding functions to assist in splitting arrays

--- template_funcs.go
+++ template_funcs.go
@@ -15,6 +15,7 @@ import (
 )
 
 var Funcs = template.FuncMap{
+	"add":		 add,
 	"allPodsReady":  allPodsReady,
 	"anyPodReady":   anyPodReady,
 	"closest":       arrayClosest,
@@ -34,6 +35,7 @@ var Funcs = template.FuncMap{
 	"isValidJson":   isValidJson,
 	"json":          marshalJson,
 	"pathJoin":      filepath.Join,
+	"pathJoinSlice": pathJoinSlice,
 	"keys":          keys,
 	"last":          last,
 	"dict":          dict,
@@ -44,6 +46,7 @@ var Funcs = template.FuncMap{
 	"readyPods":     readyPods,
 	"replace":       strings.Replace,
 	"shell":         execShell,
+	"slice":         slice,
 	"split":         strings.Split,
 	"splitN":        strings.SplitN,
 	"strContains":   strings.Contains,
@@ -57,6 +60,29 @@ var Funcs = template.FuncMap{
 	"whereNotExist": whereNotExist,
 	"whereAny":      whereAny,
 	"whereAll":      whereAll,
+}
+
+func pathJoinSlice(input []string) string {
+  return filepath.Join(input...)
+}
+
+func add(input, delta int) int {
+  return input + delta
+}
+
+// returns a slice of the input array/slice containing elements between begin (inclusive) and end (exclusive) indices
+func slice(input interface{}, begin, end int) (interface{}, error) {
+	if input == nil {
+		return input, nil
+	}
+	arr := reflect.ValueOf(input)
+	if arr.Kind() != reflect.Slice && arr.Kind() != reflect.Array {
+		return nil, fmt.Errorf("slice can only be called with slice types. received: %v", arr.Kind())
+	}
+	if arr.Len() == 0 {
+		return input, nil
+	}
+	return arr.Slice(begin, end).Interface(), nil
 }
 
 // combine multiple slices into a single slice
